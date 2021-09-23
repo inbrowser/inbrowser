cba248ab36c8c706cddd2c2948663f9f5c83fbca
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-09-24 01:15:21 +0200

    fix: fixing webpack (export to window)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    mode: "production", //todo: production
+    mode: "production", //todo: production or development
     entry: {
         seo: './seo/api.js',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        library: 'inbrowser',
+        libraryTarget: 'window'
     },
 };
