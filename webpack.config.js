59650c658e1fc978f059994bad5600d8ef030e06
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-10-01 23:52:17 +0200

    deps: updating bundles/package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,12 @@ module.exports = {
     mode: "production", //todo: production or development
     entry: {
         matrix: './matrix/api.js',
+        statistics: './statistics/api.js',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'inbrowser',
+        library: 'inbrowser_[name]',
         libraryTarget: 'window'
-    },
+    }
 };
