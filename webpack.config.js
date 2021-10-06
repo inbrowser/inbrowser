f08b15d487a8e7025fadd8cf67d6074c9d6c1967
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-10-06 23:36:08 +0200

    feat: regenerating matrix.bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
     mode: "production", //todo: production or development
     entry: {
-        matrix: './matrix/api.js',
-        statistics: './statistics/api.js',
-        calculus: './calculus/api.js',
-        algebra: './algebra/api.js',
+        // matrix: './matrix/api.js',
+        // statistics: './statistics/api.js',
+        // calculus: './calculus/api.js',
+        // algebra: './algebra/api.js',
     },
     output: {
         filename: '[name].bundle.js',
